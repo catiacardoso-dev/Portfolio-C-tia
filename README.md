24530d668880e6ec1c213aabdaecf89f37c50afc
catiacardoso-dev <catiacardosocunha@gmail.com>
2025-03-24 23:01:27 +0000

    second commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Welcome to My Portfolio 🌍
+
+## About
+
+This is my personal portfolio showcasing my projects and skills in web development. Here, you can find various projects that demonstrate my abilities in HTML, CSS, and JavaScript.
+
+## Features
+
+- **Responsive Design**: My portfolio is designed to look great on all devices.
+- **Interactive Elements**: Engaging user experience with interactive features.
+- **Easy Navigation**: Simple and intuitive navigation to explore my work.
+
+## Installation
+
+To view my portfolio locally, follow these steps:
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/catiacardoso-dev/Portfolio-C-tia
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd your-repo
+   ```
+3. Open `index.html` in your browser.
+
+## Contact
+
+Feel free to reach out to me via [email](mailto:catiacardosocunha@gmail.com) for any inquiries or collaborations!
+
+## Acknowledgments
+
+Thanks for visiting my portfolio! I hope you enjoy exploring my work. 🌍
---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById("contactForm").addEventListener("submit",  function(event) {let valid = true;
+
+// Limpar mensagem de erro
+document.getElementById("errorNome").textContent = "";
+document.getElementById("errorEmail").textContent = "";
+document.getElementById("errorMensagem").textContent = "";
+
+// Obter os valores dos campos
+let nome = document.getElementById("nome").value.trim();
+let email = document.getElementById("email").value.trim();
+let mensagem = document.getElementById("mensagem").value.trim();
+
+// Validação do nome
+if (nome=== "") {
+    document.getElementById("errorNome").textContent = "O nome é obrigatório.";valid = false;
+}
+
+// Validação do email
+let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+(!emailRegex.test(email)) {
+     document.getElementById("errorEmail").textContent = "Introduza um email válido.";valid = false;
+     }
+
+     // Validação da mensagem
+   if (mensagem.length < 10) {
+    document.getElementById("errorMensagem").textContent = "A mensagem deve ter pelo menos 10 caracteres.";valid = false;
+    }
+
+    // Se houver erro, impedir envio
+    if (!valid) {
+        event.preventDefault();
+        } else {
+            alert("Formulário enviado com sucesso!");
+        }
+{);  
